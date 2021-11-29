1991397cdc64c20dd03422c7221ae80bb27837dd
joaoleocadio <joaocarvalho388@gmail.com>
2021-11-29 23:13:42 +0000

    create user exception

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -3,12 +3,18 @@ import { CreateUserService } from "../services/CreateUserService";
 
 class CreateUserController {
   async handle(request: Request, response: Response) {
-    const { name, email, admin } = request.body;
-    const createUserService = new CreateUserService();
+    try {
+      const { name, email, admin } = request.body;
+      const createUserService = new CreateUserService();
 
-    const user = await createUserService.execute({ name, email, admin });
+      const user = await createUserService.execute({ name, email, admin });
 
-    return response.json(user);
+      return response.json(user);
+
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
+
   }
 }
 
