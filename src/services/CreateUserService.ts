d941506b2df72334d795ace3c0745286ea37fd66
joaoleocadio <joaocarvalho388@gmail.com>
2021-11-30 21:04:51 +0000

    added bcrypt

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,5 +1,6 @@
 import { getCustomRepository } from "typeorm";
 import { UsersRepositories } from "../repositories/UsersRepositories";
+import { hash } from "bcryptjs";
 
 interface IUserRequest {
   name: string;
@@ -22,7 +23,9 @@ class CreateUserService {
       throw new Error("User already exists");
     }
 
-    const user = usersRepository.create({ name, email, admin, password });
+    const passwordHash = await hash(password, 8);
+
+    const user = usersRepository.create({ name, email, admin, password: passwordHash });
 
     await usersRepository.save(user);
 
