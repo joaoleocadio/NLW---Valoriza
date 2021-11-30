f492aec06130c06402b55c27b87fb69f5cb82d71
joaoleocadio <joaocarvalho388@gmail.com>
2021-11-30 20:52:31 +0000

    added camp password to bd

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -5,10 +5,11 @@ interface IUserRequest {
   name: string;
   email: string;
   admin?: boolean;
+  password?: string;
 }
 
 class CreateUserService {
-  async execute({ name, email, admin }: IUserRequest) {
+  async execute({ name, email, admin, password }: IUserRequest) {
     const usersRepository = getCustomRepository(UsersRepositories);
 
     if (!email) {
@@ -21,7 +22,7 @@ class CreateUserService {
       throw new Error("User already exists");
     }
 
-    const user = usersRepository.create({ name, email, admin });
+    const user = usersRepository.create({ name, email, admin, password });
 
     await usersRepository.save(user);
 
