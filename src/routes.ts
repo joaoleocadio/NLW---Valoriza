82a0daeaf639a63719bb5ad10f853fee8c39ce63
joaoleocadio <joaocarvalho388@gmail.com>
2021-12-03 18:55:58 +0000

    server almost done

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,10 @@ import { ensureAdmin } from "./middlewares/ensureAdmin";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateComplimentController } from "./controllers/CreateComplimentController";
 import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
+import { ListUserSenderComplimentsController } from "./controllers/ListUserSendComplimentsController";
+import { ListUserReceiverComplimentsController } from "./controllers/ListUserReceiveComplimentsController";
+import { ListTagsController } from "./controllers/ListTagsController";
+import { ListUsersController } from "./controllers/ListUsersController";
 
 const router = Router();
 
@@ -12,10 +16,22 @@ const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 const authenticateUserController = new AuthenticateUserController();
 const createComplimentController = new CreateComplimentController();
+const listUserSendComplimentsController = new ListUserSenderComplimentsController();
+const listUserReceiveComplimentsController = new ListUserReceiverComplimentsController();
+const listsTagController = new ListTagsController();
+const listUsersController = new ListUsersController();
 
 router.post("/tags", ensureAuthenticated, ensureAdmin, createTagController.handle);
 router.post("/users", createUserController.handle);
 router.post("/login", authenticateUserController.handle);
 router.post("/compliments", ensureAuthenticated, createComplimentController.handle);
+
+router.get("/users/compliments/send", ensureAuthenticated, listUserSendComplimentsController.handle);
+router.get("/users/compliments/receive", ensureAuthenticated, listUserReceiveComplimentsController.handle);
+
+router.get("/tags", ensureAuthenticated, listsTagController.handle);
+
+router.get("/users", ensureAuthenticated, listUsersController.handle);
+
 
 export { router };
